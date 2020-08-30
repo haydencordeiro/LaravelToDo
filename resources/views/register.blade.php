0fa408a7c904ff46de2bf49b44ce5ef616ecbe04
hayden cordeiro <40896224+haydencordeiro@users.noreply.github.com>
2020-08-30 14:59:53 +0530

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>PhoneIo</title>
+  <title>List</title>
   <link rel="stylesheet" href="style2.css" />
   <link
     href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;0,700;1,400;1,500;1,700;1,900&display=swap"
